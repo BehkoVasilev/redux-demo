4c584f7922447ccda1f391f14027ceb05962f991
BehkoVasilev <vencislavvsilev@gmail.com>
2023-05-09 15:14:21 +0300

    add button for resetting the playlists

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import SongPlaylist from "./components/SongPlaylist";
 import "./styles.css";
 
 function App() {
+    const handleResetClick = () => {
+
+    }
+
     return <div className="container is-fluid">
+        <button onClick={() => handleResetClick()} className="button is-danger">
+            Reset Both Playlists
+        </button>
         <MoviePlaylist />
         <SongPlaylist />
     </div>
